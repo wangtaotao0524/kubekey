e32901e38b3b84cc192441fea6772fc709b290b5
Taotao Wang <135496506+wangtaotao0524@users.noreply.github.com>
2025-12-16 15:17:35 +0800

    Update bootstrap.go
    don't delete kubelet

--- pkg/service/bootstrap/bootstrap.go
+++ pkg/service/bootstrap/bootstrap.go
@@ -45,7 +45,7 @@ func (s *Service) AddUsers() error {
 // SetHostname sets the hostname of the machine.
 func (s *Service) SetHostname() error {
 	if _, err := s.sshClient.SudoCmdf(
-		"hostnamectl set-hostname %s && sed -i '/^127.0.1.1/s/.*/127.0.1.1      %s/g' /etc/hosts",
+		"hostnamectl set-hostname %s && sed -i '/^127.0.0.1/s/.*/127.0.0.1      %s/g' /etc/hosts",
 		s.instanceScope.HostName(),
 		s.instanceScope.HostName()); err != nil {
 		return errors.Wrapf(err, "failed to set host name [%s]", s.instanceScope.HostName())
@@ -203,8 +203,6 @@ func (s *Service) RemoveFiles() error {
 		"/run/flannel",
 		"/etc/flannel",
 		"/var/openebs",
-		"/etc/systemd/system/kubelet.service",
-		"/etc/systemd/system/kubelet.service.d",
 		"/tmp/kubekey",
 		"/etc/kubekey",
 		"/run/kubeadm/*",
